94d850debb29832d781f3fb14116ebab73e781b4
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2024-02-05 22:34:57 +0300

    Добавлен функционал

--- spigot/src/main/java/me/thedivazo/messageoverhead/core/display/LocatablePlayerSeersDisplay.java
+++
@@ -1,7 +0,0 @@
-package me.thedivazo.messageoverhead.core.display;
-
-import me.thedivazo.messageoverhead.core.message.LocatableMessage;
-import org.bukkit.entity.Player;
-
-public interface LocatablePlayerSeersDisplay<T extends LocatableMessage<?, ?, Player>> extends LocatableDisplay<T> {
-}
---
+++ spigot/src/main/java/me/thedivazo/messageoverhead/parser/WrapParser.java
@@ -0,0 +1,8 @@
+package me.thedivazo.messageoverhead.parser;
+
+/**
+ * @author TheDiVaZo
+ * created on 05.02.2024
+ */
+public interface WrapParser<T> extends Parser<T[]>{
+}
